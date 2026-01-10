f3d4bc92a9f4dfece2a4cf2f3b7055214629bf8b
Oséias Batista Pegado <oseias.batista.dev@gmail.com>
2026-01-10 15:04:02 -0300

    fx: flatpak

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,25 +7,70 @@
 
   services.flatpak = {
     enable = true;
-    packages = [
-      "com.valvesoftware.Steam"
-      "com.discordapp.Discord"
-      "com.brave.Browser"
-      "org.telegram.desktop"
-      "org.libretro.RetroArch"
-      "org.ppsspp.PPSSPP"
-      "net.pcsx2.PCSX2"
-      "net.rpcs3.RPCS3"
-      "net.shadps4.shadPS4"
-      "com.stremio.Stremio"
-      "org.gnome.Boxes"
-      "com.getpostman.Postman"
-      "io.dbeaver.DBeaverCommunity"
-      "io.github.benjamimgois.goverlay"
-      "org.kde.ktorrent"
-      "net.audiorelay.AudioRelay"
-    ];
+
+    remotes = {
+      flathub = {
+        url = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      };
+    };
   };
+
+  systemd.services.flatpak-apps = {
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "network-online.target"
+      "dbus.service"
+    ];
+    wants = [ "network-online.target" ];
+    path = [ pkgs.flatpak ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    script = ''
+      flatpak install -y --system --noninteractive flathub \
+        "com.valvesoftware.Steam" \
+        "com.discordapp.Discord" \
+        "com.brave.Browser" \
+        "org.telegram.desktop" \
+        "org.libretro.RetroArch" \
+        "org.ppsspp.PPSSPP" \
+        "net.pcsx2.PCSX2" \
+        "net.rpcs3.RPCS3" \
+        "net.shadps4.shadPS4" \
+        "com.stremio.Stremio" \
+        "org.gnome.Boxes" \
+        "com.getpostman.Postman" \
+        "io.dbeaver.DBeaverCommunity" \
+        "io.github.benjamimgois.goverlay" \
+        "org.kde.ktorrent" \
+        "net.audiorelay.AudioRelay"
+    '';
+  };
+
+  # services.flatpak = {
+  #   enable = true;
+  #   packages = [
+  #     "com.valvesoftware.Steam"
+  #     "com.discordapp.Discord"
+  #     "com.brave.Browser"
+  #     "org.telegram.desktop"
+  #     "org.libretro.RetroArch"
+  #     "org.ppsspp.PPSSPP"
+  #     "net.pcsx2.PCSX2"
+  #     "net.rpcs3.RPCS3"
+  #     "net.shadps4.shadPS4"
+  #     "com.stremio.Stremio"
+  #     "org.gnome.Boxes"
+  #     "com.getpostman.Postman"
+  #     "io.dbeaver.DBeaverCommunity"
+  #     "io.github.benjamimgois.goverlay"
+  #     "org.kde.ktorrent"
+  #     "net.audiorelay.AudioRelay"
+  #   ];
+  # };
 
   environment.systemPackages = with pkgs; [
     linuxKernel.packages.linux_zen.zenpower
