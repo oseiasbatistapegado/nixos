00d22dd9bc8963815fcc7cc5a591412f666678f1
Oséias Batista Pegado <oseias.batista.dev@gmail.com>
2026-02-01 09:48:22 -0300

    ft: update boot and dm configurations; add kernel parameters and enable redistributable firmware

--- modules/boot.nix
+++ modules/boot.nix
@@ -5,5 +5,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.blacklistedKernelModules = [ "k10temp" ];
   boot.kernelModules = [ "zenpower" ];
+  boot.kernelParams = [ "pcie_aspm=force" "amdgpu.ppfeaturemask=0xffffffff" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
 }
--- modules/dm.nix
+++ modules/dm.nix
@@ -3,8 +3,10 @@
 {
   hardware.graphics.enable32Bit = true;
   hardware.graphics.enable = true;
+  hardware.enableRedistributableFirmware = true;
 
   services.xserver.enable = false;
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   programs.xwayland.enable = true;
 
