24277cde2e3e22d9f69973c69507004bd257c5c7
Oséias Batista Pegado <oseias.batista.dev@gmail.com>
2026-02-01 10:36:57 -0300

    ft: add initial NixOS installation script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+mount /dev/disk/by-label/nixos /mnt
+mount --mkdir -o umask=077 /dev/disk/by-label/boot /mnt/boot
+mount --mkdir /dev/disk/by-label/home /mnt/home
+mount --mkdir /dev/disk/by-label/games /mnt/media/games
+
+swapon /dev/disk/by-label/swap
+
+nixos-generate-config --root /mnt
+
+cd /mnt/etc/nixos
+
+git clone https://github.com/oseiasbatistapegado/nixos.git
+
+mv -i nixos .nixos
+
+cp -r .nixos/modules .
+cp .nixos/configuration.nix .
+cp .nixos/flake.nix .
+
+nixos-install --flake '/mnt/etc/nixos#tux' --no-root-passwd
