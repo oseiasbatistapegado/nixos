003f8587813a8bdb5f53b5251914b44ae7fc77a2
Oséias Batista Pegado <oseias.batista.dev@gmail.com>
2026-02-01 14:14:23 -0300

    feat: add firewall rules for KDE Connect TCP and UDP port ranges

--- modules/network.nix
+++ modules/network.nix
@@ -4,6 +4,14 @@
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
 
+  # KDE Connect
+  networking.firewall.allowedTCPPortRanges = [
+    { from = 1714; to = 1764; }
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    { from = 1714; to = 1764; }
+  ];
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
